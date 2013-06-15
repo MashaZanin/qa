d701d0e1b4dfe7d1a2f17bc5504415ed6dad9b49
Masha Zanin <mariazanin@comcast.net>
2013-06-14 23:06:30 -0700

    update run.sh

--- ita/run.sh
+++ ita/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Commands to execute Homeworks # 13
+# Commands to execute Homeworks # 13, 14, 15, 17
 
 ruby ./hw_13/lib/script_13_01.rb apple banana
 ruby ./hw_13/lib/script_13_02.rb apples bananas
@@ -14,4 +14,15 @@ ruby ./hw_14/lib/script_14_01.rb mac
 ruby ./hw_14/lib/script_14_02.rb ipv4
 ruby ./hw_14/lib/script_14_03.rb ipv6
 ruby ./hw_15/lib/script_15_00.rb
+ruby ./hw_17/lib/script_17_01.rb -f apple -s banana
+ruby ./hw_17/lib/script_17_02.rb -f apples -s bananas
+ruby ./hw_17/lib/script_17_03.rb -f 100 -s 10
+ruby ./hw_17/lib/script_17_04.rb -a 66 -b 166 -c 202 -d 14
+ruby ./hw_17/lib/script_17_05.rb -a 35 -b 45 -c 61 -d 59 -e 73
+ruby ./hw_17/lib/script_17_06.rb -a 2 -b 4 -c 6 -d 8 -e 10 -f 12
+ruby ./hw_17/lib/script_17_06.rb -a 23 -b 15 -c 97 -d 45 -e 365 -f 1234 -g 523665 -h 45 -i 2971 -j 22145
+ruby ./hw_17/lib/script_17_07.rb -s Wednesday morning, John Smith was walking on the street.
+ruby ./hw_17/lib/script_17_07.rb -s Monday evening, Alex More was walking on the street
+ruby ./hw_17/lib/script_17_08.rb -a Spring -b Summer -c Fall -d Winter
+
 
