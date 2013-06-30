84e5c8943210d7595d29ca243cb2f8771dded8d1
Masha Zanin <mariazanin@comcast.net>
2013-06-30 14:09:08 -0700

    hw_27

---
+++ ita/hw_27/run27.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This command executes the Homework #27
+
+
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilov_page_urls.json -o ./reports/khatilov_page_urls.txt
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilov_page_urls_elements.json -o ./reports/khatilov_page_urls_elements.txt
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilov_page_urls_attributes.json -o ./reports/khatilov_page_urls_attributes.txt
+
